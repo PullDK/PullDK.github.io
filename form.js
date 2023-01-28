706f6b0482f4af42607abc5a75fd3a77c51e3ae2
PullDK <94751329+PullDK@users.noreply.github.com>
2023-01-28 14:16:43 -0300

    Delete form.js

--- form.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const mensagem = document.createElement('p');
-  mensagem.innerHTML = 'Mensagem enviada com sucesso!';
-  mensagem.style.color = 'green';
-  form.appendChild(mensagem);
-});
